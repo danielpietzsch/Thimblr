a288f9522f64c85800d2eb77dbfe2246814a17e1
Daniel Pietzsch <pietzsch.daniel@gmail.com>
2010-06-11 22:22:35 +1200

    added render_block and render_variable so far handled meta tags for custom colors and fonts and booleans

--- lib/thimblr/parser_new.rb
+++ lib/thimblr/parser_new.rb
@@ -1,7 +1,9 @@
-
 require 'yaml'
 require 'cgi'
 require 'time'
+require 'rubygems'
+require 'nokogiri'
+require 'active_support'
 
 module Thimblr
   class ParserNew
@@ -47,11 +49,63 @@ module Thimblr
     end
     
     def render_index
+      generate_meta
       return @theme
     end
     
+    # The regular expression to match a block and its contents
+    def block_regex_pattern_for(block_name)      
+      Regexp.new(/\{block:(#{block_name})\}((.|\s)*?)\{\/block:(#{block_name})\}/)
+    end
+    
+    # looks for the block named 'block_name' and replaces the whole block with just the content of the block
+    def render_block(block_name)
+      print "Rendering block {block:#{block_name}}..."
+      if @theme.gsub!(block_regex_pattern_for(block_name)) { |match| $2 }
+        puts "found and replaced!"
+      else
+        puts "no match found!"
+      end
+    end
+    
+    # removes a whole block
+    def strip_block(block_name)
+      print "Stripping block {block:#{block_name}}..."
+      if @theme.gsub!(block_regex_pattern_for(block_name), '')
+        puts "found and replaced!"
+      else
+        puts "no match found!"
+      end
+    end
+    
+    
+    def generate_meta
+      doc = Nokogiri::HTML.parse(@theme)
+      meta_elements = doc.search('meta')
+      
+      meta_elements.each do |element|
+        if element['name'].present? and element['content'].present?
+          # handling custom colors and fonts: http://www.tumblr.com/docs/en/custom_themes#appearance-options
+          if element['name'].include? 'color' or element['name'].include? 'font'
+            render_variable(element['name'], element['content'])
+          end
+          
+          # Handling Booleans: http://www.tumblr.com/docs/en/custom_themes#booleans
+          if element['name'].include? 'if:'
+            if element['content'] == "1"
+              render_block(element['name'].titlecase.gsub(/\W/, '')) # converts something like "if:Show People I Follow" to "IfShowPeopleIFollow"
+              strip_block(element['name'].titlecase.gsub(':', 'Not').gsub(/\W/, '')) # converts something like "if:Show People I Follow" to "IfNotShowPeopleIFollow"
+            else
+              strip_block(element['name'].titlecase.gsub(/\W/, ''))
+              render_block(element['name'].titlecase.gsub(':', 'Not').gsub(/\W/, ''))
+            end
+          end
+        end        
+      end # of meta_elements each
+    end # of method generate_meta
+    
     # Scans the whole theme and replaces a variable with the replacement provided
-    def replace_variable(var_name, replacement)
+    def render_variable(var_name, replacement)
       @theme.gsub!(/\{#{var_name}\}/i, replacement)
       #TODO handle variable transformations
     end
