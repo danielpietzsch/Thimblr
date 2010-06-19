d38f51a0771cd2f41844244d7c176b497335cbc2
Daniel Pietzsch <pietzsch.daniel@gmail.com>
2010-06-19 20:34:23 +1200

    added db schema

--- lib/thimblr.rb
+++ lib/thimblr.rb
@@ -7,15 +7,6 @@ require 'models/post'
 require 'models/page'
 require 'fileutils'
 
-# Database connection #################
-
-dbconfig = YAML.load(File.read('config/database.yml'))
-ActiveRecord::Base.establish_connection dbconfig['development']
-
-ActiveRecord::Base.logger = Logger.new(STDOUT)
-
-#######################################
-
 class Thimblr::Application < Sinatra::Base
   
   configure do |s|
@@ -25,6 +16,49 @@ class Thimblr::Application < Sinatra::Base
     
     enable :sessions
     set :bind, '127.0.0.1'
+    
+    
+    # Database connection #################
+
+    dbconfig = YAML.load(File.read('config/database.yml'))
+    ActiveRecord::Base.establish_connection dbconfig['development']
+
+    ActiveRecord::Base.logger = Logger.new(STDOUT)
+
+    #######################################
+    
+    begin
+    ActiveRecord::Schema.define do
+      create_table :blogs do |t|
+        t.string :title
+        t.text :description
+        t.string :name
+        t.timestamps
+      end
+      
+      create_table :posts do |t|
+        t.string :url, :url_with_slug, :post_type
+        t.datetime :date_gmt, :date
+        t.string :unix_timestamp, :format, :reblog_key, :slug
+        t.integer :width, :height, :audio_plays
+        t.string :postid
+        t.text :content
+        t.references :blog
+        t.timestamps 
+      end
+      
+      create_table :pages do |t|
+        t.string :url, :title, :link_title
+        t.boolean :render_in_theme
+        t.text :body
+        t.references :blog
+        t.timestamps
+      end
+    end
+    rescue ActiveRecord::StatementInvalid
+      puts "DB schema already exists. Not creating again."
+    end
+    
   end
 
   helpers do
