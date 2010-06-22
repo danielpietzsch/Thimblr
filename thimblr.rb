6273055676ede0bab43b27ffabcb2ae53221525b
Daniel Pietzsch <pietzsch.daniel@gmail.com>
2010-06-22 23:58:27 +1200

    added the real link to the Tumblr Themes bundle download.

--- thimblr.rb
+++ thimblr.rb
@@ -56,10 +56,10 @@ get '/' do
   redirect 'index.html'
 end
 
-get '/env' do
-  content_type 'text/plain'
-  ENV.inspect
-end
+# get '/env' do
+#   content_type 'text/plain'
+#   ENV.inspect
+# end
 
 post '/preview' do
   # TODO add error handling when no theme_code supplied or doesn't seem to be a tumblr theme
