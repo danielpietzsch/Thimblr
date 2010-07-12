80e11e844ccd5ac0fea1f0b5c50fc56af3ab5299
Daniel Pietzsch <pietzsch.daniel@gmail.com>
2010-07-12 23:20:25 +1200

    epic refactoring

--- thimblr/parser.rb
+++ thimblr/parser.rb
@@ -23,21 +23,11 @@ class Parser
   }
   
   def initialize(theme_code, blog_name = "demo")
-    @blog = Blog.find_or_import_by_name(blog_name)
-    
-    load_default_data
-    
+    @blog  = Blog.find_or_import_by_name(blog_name)    
     @theme = ThemeSnippet.new(theme_code)
-    
-    @apid = 0 # TODO rename
-  end
-  
-  # loads default data, no matter what the sample data is.
-  # this gives data from imported blogs some more stuff, since an un-authenticated API call doesn't reveal all data
-  def load_default_data
-    @following    = YAML::load(open("config/following.yml"))
-    @followed     = YAML::load(open("config/followed.yml"))
-    @groupmembers = YAML::load(open("config/groupmembers.yml"))
+    @apid  = 0 # TODO rename
+
+    load_default_data
   end
   
   def render_index
@@ -49,81 +39,42 @@ class Parser
     render_posts unless @blog.posts.blank?
     
     # This is important to be rendered AFTER render_posts,
-    # because of some ambigious variables (like Description or Title f.e.)
+    # because of some ambigious variable names (like Description or Title f.e.)
     @theme.replace_variable "Title", @blog.title
-    
-    if @blog.description.present?
-      @theme.render_block "Description"
-      @theme.replace_variable "Description", @blog.description
-      @theme.replace_variable "MetaDescription", @blog.description.gsub(/\<\/?[^\>]*\>/, "")
-    else
-      @theme.strip_block "Description"
-    end
+    render_blog_description
       
-    # Pages
-    page_template = @theme.fetch_content_of_block("Pages")
-    
-    if @blog.pages.present? and page_template.present?
-      all_pages = ThemeSnippet.new
-    
-      @blog.pages.each do |page|
-        temp = page_template.dup
-      
-        temp.replace_variable "URL", page.url
-        temp.replace_variable "Label", page.link_title
-      
-        all_pages += temp
-      end
-    
-      @theme.render_block("HasPages", all_pages)
-    else
-      @theme.strip_block "HasPages"
-    end
-
-    #pagination
-    @theme.replace_variable("CurrentPage", "1")
-    @theme.replace_variable("NextPage", "/page/2")
-    @theme.replace_variable("TotalPages", "100")
-    
-    @theme.render_block("Pagination")
-    @theme.render_block("NextPage")
-    @theme.strip_block("PreviousPage")
-    
-    render_following unless @following.blank?
-    
-    @theme.replace_variable "CopyrightYears", Defaults['CopyrightYears']
-    @theme.replace_variable "RSS", Defaults['RSS']
-    @theme.replace_variable "Favicon", Defaults['Favicon']
-    @theme.replace_variable "PortraitURL-16", Defaults['PortraitURL-16']
-    @theme.replace_variable "PortraitURL-24", Defaults['PortraitURL-24']
-    @theme.replace_variable "PortraitURL-30", Defaults['PortraitURL-30']
-    @theme.replace_variable "PortraitURL-40", Defaults['PortraitURL-40']
-    @theme.replace_variable "PortraitURL-48", Defaults['PortraitURL-48']
-    @theme.replace_variable "PortraitURL-64", Defaults['PortraitURL-64']
-    @theme.replace_variable "PortraitURL-96", Defaults['PortraitURL-96']
-    @theme.replace_variable "PortraitURL-128", Defaults['PortraitURL-128']
-    
+    render_pages
+    render_pagination
+    render_following
+    render_defaults
     disable_unsupported_stuff
     
     @theme.render_block "SearchForm"
     
+    # these blocks must not be rendered on the index page
     @theme.strip_block "PermalinkPage"
     @theme.strip_block "Permalink" # Seems to be an old version of PermalinkPage!?!
     @theme.strip_block "PostTitle"
     @theme.strip_block "PostSummary"
     
-    # cleanup stuff
-    @theme.gsub!(/\{block:([A-Za-z][A-Za-z0-9]*)\}((.|\s)*?)\{\/block:([A-Za-z][A-Za-z0-9]*)\}/i, '')
-    
-    # cleanup variables
-    @theme.gsub!(/\{([A-Za-z][A-Za-z0-9\-]*)\}/i, '')
-    
-    #cleanup rest
-    @theme.gsub!(/\{\/*?([A-Za-z][A-Za-z0-9\-:]*)\}/i, '')
+    cleanup
 
     return @theme
   end
   
+  #################################################
+  protected
+  #################################################
+  
+  # loads default data, no matter what the sample data is.
+  # this gives data from imported blogs some more stuff, since an un-authenticated API call doesn't reveal all data
+  def load_default_data
+    @following    = YAML::load(open("config/following.yml"))
+    @followed     = YAML::load(open("config/followed.yml"))
+    @groupmembers = YAML::load(open("config/groupmembers.yml"))
+  end
+  
+  # TODO divide this method into several smaller methods
   def render_posts
     posts_template = @theme.fetch_content_of_block("Posts")
   
@@ -386,6 +337,29 @@ class Parser
     @theme.render_block "Posts", all_rendered_posts
   end
   
+  # renders or removes block 'pages', depending on whether the blog has pages or not
+  def render_pages
+    # Pages
+    page_template = @theme.fetch_content_of_block "Pages"
+    
+    if @blog.pages.present? and page_template.present?
+      all_pages = ThemeSnippet.new
+    
+      @blog.pages.each do |page|
+        temp = page_template.dup
+      
+        temp.replace_variable "URL", page.url
+        temp.replace_variable "Label", page.link_title
+      
+        all_pages += temp
+      end
+    
+      @theme.render_block "HasPages", all_pages
+    else
+      @theme.strip_block "HasPages"
+    end
+  end
+  
   # stuff that is currently unsupported by thimblr
   # This also serves as a TODO list
   def disable_unsupported_stuff
@@ -448,7 +422,6 @@ class Parser
   end
   
   # handles <meta> tags Appearance Options
-  # OPTIMIZE check whether in some cases sub is sufficient (instead of gsub)
   # OPTIMIZE maybe put every option type (color, font, boolean etc.) in a separate method
   def parse_meta_options
     doc = Nokogiri::HTML.parse(@theme)
@@ -468,10 +441,10 @@ class Parser
           # converts something like "if:Show People I Follow" to "IfShowPeopleIFollow"
           @theme.render_block(element['name'].titlecase.gsub(/\W/, ''))
           # converts something like "if:Show People I Follow" to "IfNotShowPeopleIFollow"
-          @theme.strip_block(element['name'].titlecase.gsub(':', 'Not').gsub(/\W/, ''))
+          @theme.strip_block(element['name'].titlecase.sub(':', 'Not').gsub(/\W/, ''))
         else
           @theme.strip_block(element['name'].titlecase.gsub(/\W/, ''))
-          @theme.render_block(element['name'].titlecase.gsub(':', 'Not').gsub(/\W/, ''))
+          @theme.render_block(element['name'].titlecase.sub(':', 'Not').gsub(/\W/, ''))
         end
       end
       
@@ -480,9 +453,9 @@ class Parser
         if element['content'].present?
           @theme.replace_variable(element['name'], element['content'])
           # converts something like "text:Flickr Username" to "IfFlickrUsername"
-          @theme.render_block(element['name'].gsub('text', 'if').titlecase.gsub(/\W/, ''))
+          @theme.render_block(element['name'].sub('text', 'if').titlecase.gsub(/\W/, ''))
         else
-          @theme.strip_block(element['name'].gsub('text', 'if').titlecase.gsub(/\W/, ''))
+          @theme.strip_block(element['name'].sub('text', 'if').titlecase.gsub(/\W/, ''))
         end
       end
       
@@ -490,13 +463,13 @@ class Parser
       if element['name'].include? 'image:'
         if element['content'].present?
           @theme.replace_variable(element['name'], element['content'])
-          # converts something like "image:Header" Username" to "IfHeaderImage"
-          @theme.render_block(element['name'].gsub('image', 'if').titlecase.gsub(/\W/, '') + "Image")
-          # converts something like "image:Header" Username" to "IfNotHeaderImage"
-          @theme.strip_block(element['name'].gsub('image', 'if').titlecase.gsub(':', 'Not').gsub(/\W/, '') + "Image")
+          # converts something like "image:Header" to "IfHeaderImage"
+          @theme.render_block(element['name'].sub('image', 'if').titlecase.gsub(/\W/, '') + "Image")
+          # converts something like "image:Header" to "IfNotHeaderImage"
+          @theme.strip_block(element['name'].sub('image', 'if').titlecase.sub(':', 'Not').gsub(/\W/, '') + "Image")
         else
-          @theme.strip_block(element['name'].gsub('image', 'if').titlecase.gsub(/\W/, '') + "Image")
-          @theme.render_block(element['name'].gsub('image', 'if').titlecase.gsub(':', 'Not').gsub(/\W/, '') + "Image")
+          @theme.strip_block(element['name'].sub('image', 'if').titlecase.gsub(/\W/, '') + "Image")
+          @theme.render_block(element['name'].sub('image', 'if').titlecase.sub(':', 'Not').gsub(/\W/, '') + "Image")
         end
       end
     end # of meta_elements each
@@ -504,5 +477,52 @@ class Parser
     # Removing {CustomCSS}
     @theme.strip_variable("CustomCSS")
   end # of method generate_meta
-      
+  
+  def render_blog_description
+    if @blog.description.present?
+      @theme.render_block "Description"
+      @theme.replace_variable "Description", @blog.description
+      @theme.replace_variable "MetaDescription", @blog.description.gsub(/\<\/?[^\>]*\>/, "")
+    else
+      @theme.strip_block "Description"
+    end
+  end
+  
+  def render_pagination
+    @theme.replace_variable("CurrentPage", "1")
+    @theme.replace_variable("NextPage", "/page/2")
+    @theme.replace_variable("TotalPages", "100")
+    
+    @theme.render_block("Pagination")
+    @theme.render_block("NextPage")
+    @theme.strip_block("PreviousPage")
+  end
+  
+  # renders stuff which comes from the Defaults Hash
+  def render_defaults
+    @theme.replace_variable "CopyrightYears", Defaults['CopyrightYears']
+    @theme.replace_variable "RSS", Defaults['RSS']
+    @theme.replace_variable "Favicon", Defaults['Favicon']
+    @theme.replace_variable "PortraitURL-16", Defaults['PortraitURL-16']
+    @theme.replace_variable "PortraitURL-24", Defaults['PortraitURL-24']
+    @theme.replace_variable "PortraitURL-30", Defaults['PortraitURL-30']
+    @theme.replace_variable "PortraitURL-40", Defaults['PortraitURL-40']
+    @theme.replace_variable "PortraitURL-48", Defaults['PortraitURL-48']
+    @theme.replace_variable "PortraitURL-64", Defaults['PortraitURL-64']
+    @theme.replace_variable "PortraitURL-96", Defaults['PortraitURL-96']
+    @theme.replace_variable "PortraitURL-128", Defaults['PortraitURL-128']
+  end
+  
+  # remove variable and blocks that didn't get picked up
+  # or were forgotten    
+  def cleanup
+    # cleanup stuff
+    @theme.gsub!(/\{block:([A-Za-z][A-Za-z0-9]*)\}((.|\s)*?)\{\/block:([A-Za-z][A-Za-z0-9]*)\}/i, '')
+    
+    # cleanup variables
+    @theme.gsub!(/\{([A-Za-z][A-Za-z0-9\-]*)\}/i, '')
+    
+    #cleanup rest
+    @theme.gsub!(/\{\/*?([A-Za-z][A-Za-z0-9\-:]*)\}/i, '')
+  end
 end # of class
